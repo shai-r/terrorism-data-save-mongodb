ad9526a2bc34ed94e0a5a4fe1c41613a915d0744
Shai Rainer <shaira@std.hac.ac.il>
2024-12-19 11:40:13 +0200

    add schema for mongo

--- app/mongodb/database_mongo.py
+++ app/mongodb/database_mongo.py
@@ -6,8 +6,180 @@ load_dotenv(verbose=True)
 
 client = MongoClient(os.environ['MONGODB_URL'])
 db = client[os.environ['TERRORISM_DATA']]
-terrorism_collection = db['terrorism_events']
+collection_name = "terrorism_events"
 
+terrorism_collection = db.get_collection('terrorism_events')
 def restart_mongo():
-    db.drop_collection('terrorism_events')
-    db.create_collection('terrorism_events')
+    db.drop_collection(collection_name)
+    db.create_collection(
+            collection_name,
+            validator={
+                "$jsonSchema": {
+                    "bsonType": "object",
+                    "required": ["event_id", "date", "location", "weapons", "casualties", "targets", "attack_type",
+                                 "perpetrators"],
+                    "properties": {
+                        "event_id": {"bsonType": "string"},
+                        "date": {
+                            "bsonType": "object",
+                            "required": ["year", "month", "day"],
+                            "properties": {
+                                "year": {"bsonType": "int"},
+                                "month": {"bsonType": "int"},
+                                "day": {"bsonType": "int"}
+                            }
+                        },
+                        "location": {
+                            "bsonType": "object",
+                            "required": ["country", "region", "city", "latitude", "longitude"],
+                            "properties": {
+                                "country": {
+                                    "bsonType": "object",
+                                    "required": ["code", "text"],
+                                    "properties": {
+                                        "code": {"bsonType": "int"},
+                                        "text": {"bsonType": "string"}
+                                    }
+                                },
+                                "region": {
+                                    "bsonType": "object",
+                                    "required": ["code", "text"],
+                                    "properties": {
+                                        "code": {"bsonType": "int"},
+                                        "text": {"bsonType": "string"}
+                                    }
+                                },
+                                "state": {"bsonType": "string"},
+                                "city": {"bsonType": "string"},
+                                "latitude": {"bsonType": "double"},
+                                "longitude": {"bsonType": "double"}
+                            }
+                        },
+                        "weapons": {
+                            "bsonType": "array",
+                            "items": {
+                                "bsonType": "object",
+                                "required": ["type", "subtype"],
+                                "properties": {
+                                    "type": {
+                                        "bsonType": "object",
+                                        "required": ["code", "text"],
+                                        "properties": {
+                                            "code": {"bsonType": "int"},
+                                            "text": {"bsonType": "string"}
+                                        }
+                                    },
+                                    "subtype": {
+                                        "bsonType": "object",
+                                        "required": ["code", "text"],
+                                        "properties": {
+                                            "code": {"bsonType": "int"},
+                                            "text": {"bsonType": "string"}
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        "casualties": {
+                            "bsonType": "object",
+                            "required": ["killed", "wounded", "details"],
+                            "properties": {
+                                "killed": {"bsonType": "int"},
+                                "wounded": {"bsonType": "int"},
+                                "details": {
+                                    "bsonType": "object",
+                                    "properties": {
+                                        "us_killed": {"bsonType": "int"},
+                                        "terrorists_killed": {"bsonType": "int"},
+                                        "us_wounded": {"bsonType": "int"},
+                                        "terrorists_wounded": {"bsonType": "int"}
+                                    }
+                                }
+                            }
+                        },
+                        "groups": {
+                            "bsonType": "array",
+                            "items": {
+                                "bsonType": "object",
+                                "properties": {
+                                    "name": {"bsonType": "string"},
+                                    "subname": {"bsonType": "string"}
+                                }
+                            }
+                        },
+                        "targets": {
+                            "bsonType": "array",
+                            "items": {
+                                "bsonType": "object",
+                                "required": ["type", "subtype", "details", "nationality"],
+                                "properties": {
+                                    "type": {
+                                        "bsonType": "object",
+                                        "required": ["code", "text"],
+                                        "properties": {
+                                            "code": {"bsonType": "int"},
+                                            "text": {"bsonType": "string"}
+                                        }
+                                    },
+                                    "subtype": {
+                                        "bsonType": "object",
+                                        "required": ["code", "text"],
+                                        "properties": {
+                                            "code": {"bsonType": "int"},
+                                            "text": {"bsonType": "string"}
+                                        }
+                                    },
+                                    "details": {"bsonType": "string"},
+                                    "nationality": {
+                                        "bsonType": "object",
+                                        "required": ["code", "text"],
+                                        "properties": {
+                                            "code": {"bsonType": "int"},
+                                            "text": {"bsonType": "string"}
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        "attack_type": {
+                            "bsonType": "object",
+                            "required": ["primary", "secondary", "tertiary"],
+                            "properties": {
+                                "primary": {
+                                    "bsonType": "object",
+                                    "required": ["code", "text"],
+                                    "properties": {
+                                        "code": {"bsonType": "int"},
+                                        "text": {"bsonType": "string"}
+                                    }
+                                },
+                                "secondary": {
+                                    "bsonType": "object",
+                                    "required": ["code", "text"],
+                                    "properties": {
+                                        "code": {"bsonType": "int"},
+                                        "text": {"bsonType": "string"}
+                                    }
+                                },
+                                "tertiary": {
+                                    "bsonType": "object",
+                                    "required": ["code", "text"],
+                                    "properties": {
+                                        "code": {"bsonType": "int"},
+                                        "text": {"bsonType": "string"}
+                                    }
+                                }
+                            }
+                        },
+                        "perpetrators": {
+                            "bsonType": "object",
+                            "required": ["count", "captured"],
+                            "properties": {
+                                "count": {"bsonType": "int"},
+                                "captured": {"bsonType": "int"}
+                            }
+                        }
+                    }
+                }
+            }
+        )
--- app/services/mongo_service.py
+++ app/services/mongo_service.py
@@ -1,24 +1,39 @@
+def convert_to_int(value):
+    if value.isdigit():
+        return int(value)
+    return 0
+
+def convert_to_float(value):
+    try:
+        return float(value)
+    except ValueError:
+        return 0.0
+
+
 def transform_data_to_record(raw_data):
     return [{
         "event_id": row["eventid"],
         "date": {
-            "year": row["iyear"],
-            "month": row["imonth"],
-            "day": row["iday"],
+            "year": convert_to_int(row["iyear"]),
+            "month": convert_to_int(row["imonth"]),
+            "day": convert_to_int(row["iday"]),
         },
         "location": {
-            "country": {"code": row["country"], "text": row["country_txt"]},
-            "region": {"code": row["region"], "text": row["region_txt"]},
+            "country": {"code": convert_to_int(row["country"]), "text": row["country_txt"]},
+            "region": {"code": convert_to_int(row["region"]), "text": row["region_txt"]},
             "state": row.get("provstate"),
             "city": row.get("city"),
-            "latitude": row.get("latitude"),
-            "longitude": row.get("longitude"),
+            "latitude": convert_to_float(row.get("latitude")),
+            "longitude": convert_to_float(row.get("longitude")),
         },
         "weapons": [
             {
-                "type": {"code": row[f"weaptype{i}"], "text": row[f"weaptype{i}_txt"]},
+                "type": {
+                    "code": convert_to_int(row[f"weaptype{i}"]),
+                    "text": row[f"weaptype{i}_txt"]
+                },
                 "subtype": {
-                    "code": row.get(f"weapsubtype{i}"),
+                    "code": convert_to_int(row.get(f"weapsubtype{i}")),
                     "text": row.get(f"weapsubtype{i}_txt"),
                 },
             }
@@ -26,33 +41,33 @@ def transform_data_to_record(raw_data):
             if row.get(f"weaptype{i}") and row.get(f"weaptype{i}_txt")
         ],
         "casualties": {
-            "killed": row.get("nkill"),
-            "wounded": row.get("nwound"),
+            "killed": convert_to_int(row.get("nkill")),
+            "wounded": convert_to_int(row.get("nwound")),
             "details": {
-                "us_killed": row.get("nkillus"),
-                "terrorists_killed": row.get("nkillter"),
-                "us_wounded": row.get("nwoundus"),
-                "terrorists_wounded": row.get("nwoundte"),
+                "us_killed": convert_to_int(row.get("nkillus")),
+                "terrorists_killed": convert_to_int(row.get("nkillter")),
+                "us_wounded": convert_to_int(row.get("nwoundus")),
+                "terrorists_wounded": convert_to_int(row.get("nwoundte")),
             },
         },
         "groups": [
             {
-                "name": row.get(f"gname{i}"),
-                "subname": row.get(f"gsubname{i}"),
+                "name": row.get(f"gname{i if i > 1 else ""}"),
+                "subname": row.get(f"gsubname{i if i > 1 else ""}"),
             }
             for i in range(1, 4)
-            if row.get(f"gname{i}")
+            if row.get(f"gname{i if i > 1 else ""}")
         ],
         "targets": [
             {
-                "type": {"code": row[f"targtype{i}"], "text": row[f"targtype{i}_txt"]},
+                "type": {"code": convert_to_int(row[f"targtype{i}"]), "text": row[f"targtype{i}_txt"]},
                 "subtype": {
-                    "code": row.get(f"targsubtype{i}"),
+                    "code": convert_to_int(row.get(f"targsubtype{i}")),
                     "text": row.get(f"targsubtype{i}_txt"),
                 },
                 "details": row.get(f"target{i}"),
                 "nationality": {
-                    "code": row.get(f"natlty{i}"),
+                    "code": convert_to_int(row.get(f"natlty{i}")),
                     "text": row.get(f"natlty{i}_txt"),
                 },
             }
@@ -60,18 +75,18 @@ def transform_data_to_record(raw_data):
             if row.get(f"targtype{i}") and row.get(f"targtype{i}_txt")
         ],
         "attack_type": {
-            "primary": {"code": row["attacktype1"], "text": row["attacktype1_txt"]},
+            "primary": {"code": convert_to_int(row["attacktype1"]), "text": row["attacktype1_txt"]},
             "secondary": {
-                "code": row.get("attacktype2"),
+                "code": convert_to_int(row.get("attacktype2")),
                 "text": row.get("attacktype2_txt"),
             },
             "tertiary": {
-                "code": row.get("attacktype3"),
+                "code": convert_to_int(row.get("attacktype3")),
                 "text": row.get("attacktype3_txt"),
             },
         },
         "perpetrators": {
-            "count": row.get("nperps"),
-            "captured": row.get("nperpcap"),
+            "count": convert_to_int(row.get("nperps")),
+            "captured": convert_to_int(row.get("nperpcap")),
         },
     }  for row in raw_data ]
